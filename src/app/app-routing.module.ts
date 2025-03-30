3f0eb41165408f59ae2556540ad9b7bed633c2b4
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-03-30 13:19:15 -0400

    Instalação e configuração Tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }, { path: 'detalhe', loadChildren: () => import('./modules/detalhe/detalhe.module').then(m => m.DetalheModule) }, { path: 'informacoes', loadChildren: () => import('./modules/informacoes/informacoes.module').then(m => m.InformacoesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
