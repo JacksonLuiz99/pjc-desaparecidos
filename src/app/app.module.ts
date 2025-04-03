cb4a5b04f785dadc4c3221aeab8589c4e85d01bf
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-04-03 10:13:18 -0400

    Create footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ButtonComponent } from './components/button/button.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,15 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ButtonComponent]
-    });
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +17,18 @@ describe('ButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit clickEvent when clicked and not disabled', () => {
+    spyOn(component.clickEvent, 'emit');
+    component.handleClick(new Event('click'));
+    expect(component.clickEvent.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit clickEvent when disabled', () => {
+    spyOn(component.clickEvent, 'emit');
+    component.disabled = true;
+    component.handleClick(new Event('click'));
+    expect(component.clickEvent.emit).not.toHaveBeenCalled();
   });
 });
