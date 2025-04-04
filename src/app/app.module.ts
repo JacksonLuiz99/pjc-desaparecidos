d15e4ad963afc77f862e717c859acd6f9022d3ff
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-04-03 20:18:08 -0400

    update lista-desaparecidos, footer e header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
     AppComponent,
     ButtonComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
